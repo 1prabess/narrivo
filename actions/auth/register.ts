2f1bffd92e6a07159ca29d41b9dab023dc2cfdb1
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-22 15:37:37 +0545

    Add user registration action
    Introduces signUp function to handle user registration, including field validation, email uniqueness check, password hashing, and user creation in the database.

---
+++ actions/auth/register.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/lib/user";
+import { RegisterSchema, RegisterSchemaType } from "@/schemas/RegisterSchema";
+import bcrypt from "bcryptjs";
+
+export const signUp = async (values: RegisterSchemaType) => {
+  const validateFields = RegisterSchema.safeParse(values);
+
+  if (!validateFields.success) return { error: "Invalid Fields!" };
+
+  const { name, email, password } = validateFields.data;
+
+  const user = await getUserByEmail(email);
+
+  if (user) return { error: "Email already in use!" };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created successfully!" };
+};
