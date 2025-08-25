117915e50295d85085649bb3fe17befac7f305ea
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-25 14:11:44 +0545

    Add social login functionality to SocialAuth
    Implemented login handlers for Google and Github using next-auth's signIn method. Buttons now trigger authentication and redirect to LOGIN_REDIRECT after successful sign-in.

--- components/auth/SocialAuth.tsx
+++ components/auth/SocialAuth.tsx
@@ -1,7 +1,15 @@
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { signIn } from "next-auth/react";
 import Button from "../common/Button";
+import { LOGIN_REDIRECT } from "@/routes";
 
 const SocialAuth = () => {
+  const handleLogin = (provider: "google" | "github") => {
+    signIn(provider, {
+      redirectTo: LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex justify-between flex-col md:flex-row">
       <Button
@@ -9,7 +17,7 @@ const SocialAuth = () => {
         label="Continue with Github"
         outlined
         icon={FaGithub}
-        onClick={() => {}}
+        onClick={() => handleLogin("github")}
       />
 
       <Button
@@ -17,7 +25,7 @@ const SocialAuth = () => {
         label="Continue with Google"
         outlined
         icon={FaGoogle}
-        onClick={() => {}}
+        onClick={() => handleLogin("google")}
       />
     </div>
   );
