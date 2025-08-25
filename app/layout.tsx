b6d41335138d29d14ea398cbd59d7ce7584d012f
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-25 14:12:05 +0545

    Add session provider to root layout
    Wrapped the root layout with next-auth's SessionProvider and integrated session retrieval using the auth function. This enables authentication context throughout the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/layout/Navbar";
 import { ThemeProvider } from "next-themes";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
 
 const playFair = Playfair({
   variable: "--font-play-fair",
@@ -15,30 +17,34 @@ export const metadata: Metadata = {
   description: "Number 1 Blog App",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "antialiased flex flex-col min-h-screen px-2",
-          playFair.variable
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <SessionProvider session={session}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "antialiased flex flex-col min-h-screen px-2",
+            playFair.variable
+          )}
         >
-          <Navbar />
-          <main className="flex-grow">{children}</main>
-          <footer>footer</footer>
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            <main className="flex-grow">{children}</main>
+            <footer>footer</footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
