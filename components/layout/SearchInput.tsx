93f8cbd28ca74e7f1965b868da9c0a221cbf43ef
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-25 14:11:30 +0545

    Fix typo in SearchInput icon class
    Corrected '-tranneutral-y-1/2' to '-translate-y-1/2' in the Search icon's className to properly position the icon vertically.

--- components/layout/SearchInput.tsx
+++ components/layout/SearchInput.tsx
@@ -4,7 +4,7 @@ import { Input } from "../ui/input";
 const SearchInput = () => {
   return (
     <div className="relative w-72">
-      <Search className="absolute left-3 top-1/2 -tranneutral-y-1/2 text-neutral-400" />
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-neutral-400" />
       <Input className="pl-12 rounded-full " placeholder="Search" />
     </div>
   );
