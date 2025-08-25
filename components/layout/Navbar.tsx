9f5347528d11950c7b0e0e8965d07034c917b248
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-25 14:11:38 +0545

    Update Navbar to show user options based on auth state
    Navbar now conditionally displays Notifications and UserButton only for authenticated users, and shows Login/Register links for unauthenticated users. Added session and pathname hooks to manage authentication state and trigger session updates on route changes.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import Container from "./Container";
 import Notifications from "./Notifications";
 import SearchInput from "./SearchInput";
 import ThemeToggle from "./ThemeToggle";
 import UserButton from "./UserButton";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const session = useSession();
+  const isLoggedIn = session.status === "authenticated";
+  const path = usePathname();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      const updateSession = async () => {
+        await session.update();
+      };
+
+      updateSession();
+    }
+  }, [path]);
+
   return (
     <nav className="sticky top-0 border-b z-50 bg-white dark:bg-neutral-950 ">
       <Container>
@@ -16,12 +35,14 @@ const Navbar = () => {
           <SearchInput />
           <div className="flex gap-5 sm:gap-8 items-center">
             <ThemeToggle />
-            <Notifications />
-            <UserButton />
-            <>
-              <Link href={"/login"}>Login</Link>
-              <Link href={"/register"}>Register</Link>
-            </>
+            {isLoggedIn && <Notifications />}
+            {isLoggedIn && <UserButton />}
+            {!isLoggedIn && (
+              <>
+                <Link href={"/login"}>Login</Link>
+                <Link href={"/register"}>Register</Link>
+              </>
+            )}
           </div>
         </div>
       </Container>
