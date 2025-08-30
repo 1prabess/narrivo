80b2071b456e505af98f771e691e9a32b8448752
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-08-30 14:53:31 +0545

    Add EdgeStoreProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/layout/Navbar";
 import { ThemeProvider } from "next-themes";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const playFair = Playfair({
   variable: "--font-play-fair",
@@ -25,26 +26,28 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
-    <SessionProvider session={session}>
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(
-            "antialiased flex flex-col min-h-screen px-2",
-            playFair.variable
-          )}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+    <EdgeStoreProvider>
+      <SessionProvider session={session}>
+        <html lang="en" suppressHydrationWarning>
+          <body
+            className={cn(
+              "antialiased flex flex-col min-h-screen px-2",
+              playFair.variable
+            )}
           >
-            <Navbar />
-            <main className="flex-grow">{children}</main>
-            <footer>footer</footer>
-          </ThemeProvider>
-        </body>
-      </html>
-    </SessionProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navbar />
+              <main className="flex-grow">{children}</main>
+              <footer>footer</footer>
+            </ThemeProvider>
+          </body>
+        </html>
+      </SessionProvider>
+    </EdgeStoreProvider>
   );
 }
